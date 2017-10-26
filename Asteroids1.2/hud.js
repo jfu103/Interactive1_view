6f9be1f9435635e5ee07cf3c28cd7b222c7a79c7
jfu103 <31708868+jfu103@users.noreply.github.com>
2017-10-26 04:19:51 -0700

    attempt 1 at upload

---
+++ Asteroids1.2/hud.js
@@ -0,0 +1,122 @@
+function Hud() {
+  var size = 20;
+  var padding = 10;
+  var lifeWidth = 20;
+
+  // digitMaps is used to create line representations of digits 0 through 9,
+  // the diagram below indicates the mapping of the digitMaps array index to
+  // its visual line.
+  /*
+   --0--
+   1   2
+   --3--
+   4   5
+   --6--
+  */
+//  var digitMaps = [
+//    // Return a digit map
+//    [true, true, true, false, true, true, true], //0
+//    [false, false, true, false, false, true, false], //1
+//    [true, false, true, true, true, false, true], //2
+//    [true, false, true, true, false, true, true], //3
+//    [false, true, true, true, false, true, false], //4
+//    [true, true, false, true, false, true, true], //5
+//    [true, true, false, true, true, true, true], //6
+//    [true, false, true, false, false, true, false], //7
+//    [true, true, true, true, true, true, true], //8
+//    [true, true, true, true, false, true, true] //9
+//
+//  ];
+
+  this.render = function() {
+//    stroke(200,250,50);
+//    fill(0,0,0);
+////    rect(20,height-170,width-40,150);
+//    textSize(16);
+//    fill(255);
+//    text("X Speed", 40, height-100);
+//    text(ship.vel.x, 40, height-80);
+//    text("Y Speed", 240, height-100);
+//    text(ship.vel.y, 240, height-80);
+//    var scoreString = "" + score;
+//    var x = (width - (scoreString.length * (size + padding))) / 2;
+//    var digitPos = createVector(x, padding);
+//    for(var i = 0; i < scoreString.length; i++) {
+//      var dmap = digitMaps[scoreString.charAt(i)];
+//      drawDigit(dmap, i, digitPos);
+//      digitPos.x += size + padding;
+//    }
+
+	  
+	  
+    drawLives();
+	 
+    if(lives < 0) { //IMPORTANT
+      push();
+//      textSize(15);
+//      fill(255);
+//	let finalMsg = ["Too bad! Being unable to increase your stats in order to ensure better survival sure is irritating, isn’t it? ","Hmm. Seems like you ran out of luck. Too bad gene editing isn’t a thing in this game, like it is in real life. ","If only your survival didn't rely on some random variables that you have no control over, right?"];
+//		console.log(randomIndex);
+		
+//      text(finalMsg[randomIndex], (width / 2) - 100, height / 2, 200, 400);
+		image(finalImg[randomIndex], (width/2), height/2);
+		imageMode(CENTER);
+	} 
+  }
+
+  
+  function drawLives() {
+    push();
+    stroke(255);
+    fill(0);
+    var top = createVector((width-20), 50, 100,100);
+    for(var i = 0; i < lives; i++) {
+      triangle(top.x,                 top.y     ,
+               top.x - lifeWidth / 2, top.y + 25,
+               top.x + lifeWidth / 2, top.y + 25);
+      top.x -= 20 + padding;
+    }
+    pop();
+  }
+
+  //draws the digit based on the digit map
+  function drawDigit(digitMap, index, pos) {
+    push();
+    stroke(255);
+    for(var i = 0; i < digitMap.length; i++) {
+      if(digitMap[i] === true)
+        drawLine(i, pos);
+    }
+    pop();
+  }
+
+  //draws a line based on the line map
+  function drawLine(lineMap, pos) {
+    switch(lineMap) {
+      case 0:
+        line(pos.x, pos.y, pos.x + size, pos.y);
+        break;
+      case 1:
+        line(pos.x, pos.y, pos.x, pos.y + size);
+        break;
+      case 2:
+        line(pos.x + size, pos.y, pos.x + size, pos.y + size);
+        break;
+      case 3:
+        line(pos.x, pos.y + size, pos.x + size, pos.y + size);
+        break;
+      case 4:
+        line(pos.x, pos.y + size, pos.x, pos.y + 2 * size);
+        break;
+      case 5:
+        line(pos.x + size, pos.y + size, pos.x + size, pos.y + 2 * size);
+        break;
+      case 6:
+        line(pos.x, pos.y + size * 2, pos.x + size, pos.y + 2 * size);
+        break;
+      default:
+        console.log("line map is invalid");
+        break;
+    }
+  }
+}
---
+++ Asteroids1.2/sketch.js
@@ -0,0 +1,218 @@
+// Daniel Shiffman
+// http://codingrainbow.com
+// http://patreon.com/codingrainbow
+// Code for: https://youtu.be/hacZU523FyM
+
+var ship;
+var hud;
+var asteroids = [];
+var lasers = [];
+var laserSoundEffects = [];
+var dust = [];
+var explosionSoundEffects = [];
+var canPlay = true;
+var shieldTime = 180;
+var imgGoodGuy;
+var asteroidBig;
+var randomIndex;
+//var scoreImg;
+var counter;
+var chooseNumber;
+var finalImg = [];
+var cosmicAlien;
+let years = 0;
+
+
+
+function preload() {
+
+	//array for death messages
+	finalImg[0] = loadImage("assets/msg0.png");
+	finalImg[1] = loadImage("assets/msg1.png");
+	finalImg[2] = loadImage("assets/msg2.png");
+
+	
+	//load font Cosmic Alien by Freaky Fonts 
+
+	cosmicAlien = loadFont("assets/ca.otf")
+
+	// Laser and Explosion Sound Effects are loaded here as opposed to the laser
+	// or asteroid files because the asteroid destruction logic is here and it
+	// also reduces redundancy of each asteroid or laser containing sound data.
+	for (var i = 0; i < 3; i++) {
+		laserSoundEffects[i] = loadSound('audio/pew-' + i + '.mp3');
+	}
+	for (var i = 0; i < 3; i++) {
+		explosionSoundEffects[i] = loadSound('audio/explosion-' + i + '.mp3');
+	}
+
+	for (var i = 0; i < 3; i++) {
+		imgGoodGuy = loadImage("assets/good-guy.png");
+	}
+
+	for (var i = 0; i < 3; i++) {
+		asteroidBig = loadImage("assets/asteroid.png");
+	}
+	//	scoreImg = loadImage("assets/score.png");
+}
+var score = 0;
+var lives = 3;
+var points = [100, 50, 20]; // small, med, large points
+var level = 0;
+
+function setup() {
+
+	//COUNTERsetup
+
+	counter = 0;
+	createCanvas(windowWidth, windowHeight);
+	ship = new Ship();
+	hud = new Hud();
+	spawnAsteroids();
+	randomIndex = floor(random(3));
+
+	//font setup
+
+	textFont(cosmicAlien);
+	//text
+
+	//YEARS counting
+	function timeIt() {
+		years++;
+	}
+
+	setInterval(timeIt, 5);
+
+}
+
+
+function draw() {
+	//COUNTERdraw
+
+	counter++;
+	if (counter == 60) {
+		counter = 0;
+
+	}
+	// Handles the round loss, destruction of ship and round restart when the
+	// ship contacts an asteroid.
+	for (var i = 0; i < asteroids.length; i++) {
+		if (ship.hits(asteroids[i]) && canPlay) {
+			canPlay = false;
+			ship.destroy();
+			input.reset();
+			setTimeout(function () {
+				lives--;
+				if (lives >= 0) {
+					ship = new Ship();
+					canPlay = true;
+				}
+			}, 3000);
+		}
+		asteroids[i].update();
+	}
+
+	// Update the lasers' positions
+	for (var i = lasers.length - 1; i >= 0; i--) {
+		lasers[i].update();
+		if (lasers[i].offscreen()) {
+			// Destroy lasers that go off screen.
+			lasers.splice(i, 1);
+
+			continue;
+		}
+
+		for (var j = asteroids.length - 1; j >= 0; j--) {
+			if (lasers[i].hits(asteroids[j])) {
+				// Handle laser contact with asteroids - handles graphics and sounds -
+				// including asteroids that result from being hit.
+				asteroids[j].playSoundEffect(explosionSoundEffects);
+				score += points[asteroids[j].size];
+				var dustVel = p5.Vector.add(lasers[i].vel.mult(0.2), asteroids[j].vel);
+				var dustNum = (asteroids[j].size + 1) * 5;
+				addDust(asteroids[j].pos, dustVel, dustNum);
+				// The new smaller asteroids broken lasers are added to the same list
+				// of asteroids, so they can be referenced the same way as their full
+				// asteroid counterparts.
+				var newAsteroids = asteroids[j].breakup();
+				asteroids = asteroids.concat(newAsteroids);
+				// Laser and previous asteroid are removed as per the rules of the game.
+				asteroids.splice(j, 1);
+				lasers.splice(i, 1);
+				if (asteroids.length == 0) {
+					// Next level
+					level++;
+					spawnAsteroids();
+					ship.shields = shieldTime;
+				}
+				break;
+			}
+		}
+	}
+
+	ship.update();
+
+	for (var i = dust.length - 1; i >= 0; i--) {
+		dust[i].update();
+		if (dust[i].transparency <= 0) {
+			dust.splice(i, 1);
+		}
+	}
+
+	// Render
+	background(0);
+
+	for (var i = 0; i < asteroids.length; i++) {
+		asteroids[i].render();
+	}
+
+	for (var i = lasers.length - 1; i >= 0; i--) {
+		lasers[i].render();
+	}
+
+	ship.render();
+	hud.render();
+
+	for (var i = dust.length - 1; i >= 0; i--) {
+		dust[i].render();
+	}
+	//DRAW TEXT 
+	textSize(20);
+	text("Viruses Eliminated", (width - 660), 5, 100, 100);
+	text("Years Elapsed", (width - 300), 5, 100, 100);
+	textAlign(RIGHT);
+	textSize(50);
+	//score draw
+	text(score, (width - 410), 40);
+	fill(255);
+	noStroke();
+	text(years, (width - 10), 40);
+	textSize(12);
+
+}
+
+function spawnAsteroids() {
+	for (var i = 0; i < level + 20*random(10); i++) { //IMPORTANT # OF ENEMIESf
+		asteroids.push(new Asteroid(null, null, 2));
+	}
+}
+
+function cross(v1, v2) {
+	return v1.x * v2.y - v2.x * v1.y;
+}
+
+function lineIntersect(l1v1, l1v2, l2v1, l2v2) {
+	var base = p5.Vector.sub(l1v1, l2v1);
+	var l1_vector = p5.Vector.sub(l1v2, l1v1);
+	var l2_vector = p5.Vector.sub(l2v2, l2v1);
+	var direction_cross = cross(l2_vector, l1_vector);
+	var t = cross(base, l1_vector) / direction_cross;
+	var u = cross(base, l2_vector) / direction_cross;
+	if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+// count up timer p5 modified from https://codepen.io/pleabargain/pen/vxaday?editors=0010 - user dgd
