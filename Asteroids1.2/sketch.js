726d38402ec99a613484ddf00e249b5b04fea81a
jfu103 <31708868+jfu103@users.noreply.github.com>
2017-10-26 21:06:27 -0700

    update assets

--- Asteroids1.2/sketch.js
+++ Asteroids1.2/sketch.js
@@ -203,7 +203,7 @@ function draw() {
 	textSize(12);
 	fill(255);
 	
-	text("Your DNA replicates all the time, but it only gives you significant mutations about once every 50 years. By significant, we mean 'not necessarily helpful'.", width-1150,5, 400, 100);
+	text("Features such as movement may take time to develop. Your DNA replicates all the time, but it only gives you significant mutations about once every 50 years. Significant, but not necessarily helpful.", width-1150,5, 400, 100);
 
 }
 
