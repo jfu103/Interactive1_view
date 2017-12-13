5c13015cd9c8f892dd73d3841f0193f0dc102d54
jfu103 <31708868+jfu103@users.noreply.github.com>
2017-12-13 09:44:22 -0800

    project 3 index page link update

--- Project_3/sketch.js
+++ Project_3/sketch.js
@@ -1,38 +1,75 @@
 // Sticky Header
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 100) {
-    $(".main_h").addClass("sticky");
-  } else {
-    $(".main_h").removeClass("sticky");
-  }
-});
 
-// Mobile Navigation
-$(".mobile-toggle").click(function() {
-  if ($(".main_h").hasClass("open-nav")) {
-    $(".main_h").removeClass("open-nav");
-  } else {
-    $(".main_h").addClass("open-nav");
-  }
-});
-
-$(".main_h li a").click(function() {
-  if ($(".main_h").hasClass("open-nav")) {
-    $(".navigation").removeClass("open-nav");
-    $(".main_h").removeClass("open-nav");
-  }
-});
-
-// navigation scroll lijepo radi materem
-$("nav a").click(function(event) {
-  var id = $(this).attr("href");
-  var offset = 70;
-  var target = $(id).offset().top - offset;
-  $("html, body").animate(
-    {
-      scrollTop: target
-    },
-    500
-  );
-  event.preventDefault();
-});
+$(document).ready(function(){
+//	$(window).scroll(function() {
+//	  if ($(window).scrollTop() > 100) {
+//		$(".main_h").addClass("sticky");
+//	  } else {
+//		$(".main_h").removeClass("sticky");
+//	  }
+//	});
+//
+//	// Mobile Navigation
+//	$(".mobile-toggle").click(function() {
+//	  if ($(".main_h").hasClass("open-nav")) {
+//		$(".main_h").removeClass("open-nav");
+//	  } else {
+//		$(".main_h").addClass("open-nav");
+//	  }
+//	});
+//
+//	$(".main_h li a").click(function() {
+//	  if ($(".main_h").hasClass("open-nav")) {
+//		$(".navigation").removeClass("open-nav");
+//		$(".main_h").removeClass("open-nav");
+//	  }
+//	});
+//
+//	// navigation scroll lijepo radi materem
+//	$("nav a").click(function(event) {
+//	  var id = $(this).attr("href");
+//	  var offset = 70;
+//	  var target = $(id).offset().top - offset;
+//	  $("html, body").animate(
+//		{
+//		  scrollTop: target
+//		},
+//		500
+//	  );
+//	  event.preventDefault();
+//	});
+	
+	
+	$('a').click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+	
+})
